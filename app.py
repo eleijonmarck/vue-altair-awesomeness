3955400f2f1eca16a1129ef885126c4d1560a491
eleijonmarck <eric.leijonmarck@gmail.com>
2019-02-11 11:59:41 +0100

    added more graphs

--- app.py
+++ app.py
@@ -8,23 +8,77 @@ alt.data_transformers.enable('default', max_rows=None)
 
 app = Flask(__name__)
 
+df = pd.read_csv('data/2018_08_17_0013_2018-08-17--high-reti.csv.gz')
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/vega-example')
-def vega():
+def example():
 
-    df = pd.read_csv('2018_08_17_0013_2018-08-17--high-reti.csv.gz')
+    attributes = [
+        'FUEL_FLOW_1:Q', 
+        'FUEL_FLOW_2:Q', 
+    ]
 
-    chart = alt.Chart(df).mark_point().encode(
-                x='flight_datetime:T',
-                y='ALTITUDE_STANDARD:Q',
-            )
+    base = alt.Chart().mark_line().encode(
+        x='flight_datetime:T',
+    )
+
+    chart = alt.hconcat(data=df)
+
+    for y_encoding in attributes:
+        row = base.encode(
+            y=y_encoding,
+        )
+        
+        chart |= row
+        
     return jsonify(
-            chart.to_dict()
-           )
+        chart.to_dict()
+    )
+
+@app.route('/vega-altitude')
+def vega_altitude():
+
+    attributes = [
+        'ALTITUDE_STANDARD:Q', 
+        'ALTITUDE_BARO:Q', 
+    ]
+
+    base = alt.Chart().mark_line().encode(
+        x='flight_datetime:T',
+    )
+
+    chart = alt.hconcat(data=df)
+
+    for y_encoding in attributes:
+        row = base.encode(
+            y=y_encoding,
+        )
+        
+        chart |= row
+        
+    return jsonify(
+        chart.to_dict()
+    )
+
+@app.route('/vega-latlong')
+def latlong():
+
+    points = alt.Chart(
+        df
+    ).mark_circle().encode(
+        longitude='longitude:Q',
+        latitude='latitude:Q',
+        size=alt.value(10),
+        color=alt.value('steelblue')
+    )
+       
+    return jsonify(
+        points.to_dict()
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
-
